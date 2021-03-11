5f52b1f98ca820cb0a4270907f5ea72b69a2a87f
artseven <yabkevich.artsiom.09@gmail.com>
2021-03-11 16:27:40 -0500

    displaying build controls on main page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,23 +1,23 @@
 import { Component } from "react";
 import Aux from "../../hoc/Auxiliary";
-import Burger from '../../components/Burger/Burger';
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
-
   state = {
     ingredients: {
       salad: 0,
       bacon: 0,
       cheese: 0,
-      meat: 0
-    }
-  }
+      meat: 0,
+    },
+  };
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Aux>
     );
   }
