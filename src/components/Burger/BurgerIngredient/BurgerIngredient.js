f7be1e5e58a76724c323ca2097de9405476ac905
artseven <yabkevich.artsiom.09@gmail.com>
2021-03-11 15:47:06 -0500

    fixed typos

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -13,27 +13,28 @@ class BurgerIngredient extends Component {
         break;
       case "bread-top":
         ingredient = (
-          <div className="classes.BreadTop">
-            <div className="classes.Seeds1"></div>
-            <div className="classes.Seeds2"></div>
+          <div className={classes.BreadTop}>
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
           </div>
         );
         break;
       case "meat":
-        ingredient = <div className="classes.Meat"></div>;
+        ingredient = <div className={classes.Meat}></div>;
         break;
       case "cheese":
-        ingredient = <div className="classes.Cheese"></div>;
+        ingredient = <div className={classes.Cheese}></div>;
         break;
       case "salad":
-        ingredient = <div className="classes.Salad"></div>;
+        ingredient = <div className={classes.Salad}></div>;
         break;
       case "bacon":
-        ingredient = <div className="classes.Bacon"></div>;
+        ingredient = <div className={classes.Bacon}></div>;
         break;
       default:
         ingredient = null;
     }
+    console.log(ingredient);
     return ingredient;
   }
 }
